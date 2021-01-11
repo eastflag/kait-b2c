a3df72a5cc9c978a874dd88cc5ba41ac8d0ff510
eastflag <dklee98@>
2021-01-11 19:02:22 +0900

    .

--- src/components/SubjectiveQuestion.jsx
+++ src/components/SubjectiveQuestion.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Latex from "react-latex";
 
-function SubjectiveQuestion({index}) {
+function SubjectiveQuestion({equation, index}) {
   console.log('SubjectiveQuestion', index)
+  const [input, setInput] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  useEffect(() => {
+    setAnswer(equation);
+  }, [])
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    const inputList = e.target.value.split(',');
+    let copyEquation = equation;
+    inputList.forEach(item => {
+      if (item) {
+        copyEquation = copyEquation.replace('@', item);
+      }
+    })
+    console.log(copyEquation);
+    setAnswer(copyEquation)
+  }
+
   return (
     <div>
-      <input type="text"/>
+      <Latex displayMode={true}>{`\$\$${answer}\$\$`}</Latex>
+      <div>
+        <input type="text" onChange={onChange}/>
+      </div>
     </div>
   );
 }
