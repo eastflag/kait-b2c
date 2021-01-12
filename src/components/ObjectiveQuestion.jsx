b25b98da3b54fcde433c189a343f70d773183d24
eastflag <dklee98@>
2021-01-12 14:10:50 +0900

    객관식문제 - 다지선다 로직 추가

--- src/components/ObjectiveQuestion.jsx
+++ src/components/ObjectiveQuestion.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Col, Button} from "antd";
 
 function ObjectiveQuestion({example, answer, index}) {
-  console.log('ObjectiveQuestion', index);
-  return example.replace(' ', '').split(',').map((item, index) => <Col><Button key={index}>{item}</Button></Col>)
+  const [myAnswer, setMyAnswer] = useState('');
+  const answerCount = answer.split(',').length;
+
+  const onToggle = (number) => {
+    if (answerCount > 1) {
+      let tempArray = myAnswer ? myAnswer.split(',') : [];
+      if (myAnswer.indexOf(number) > -1) {
+        const index = tempArray.findIndex(item => item === number);
+        tempArray.splice(index, 1);
+        setMyAnswer(tempArray.length === 0 ? '' : tempArray.join(','));
+      } else {
+        tempArray.push(number);
+        tempArray.sort();
+        setMyAnswer(tempArray.join(','));
+      }
+    } else {
+      setMyAnswer(number);
+    }
+  }
+
+  return example.replaceAll(' ', '')
+    .split(',').map((item, index) => <Col><Button type={myAnswer.indexOf(item) > -1 ? 'primary' : ''} key={index} onClick={() => onToggle(item)}>{item}</Button></Col>)
 }
 
 export default ObjectiveQuestion;
