55f0701d7547890c9fb316f4b2a151bfc97e804a
eastflag <dklee98@>
2021-04-15 17:03:36 +0900

    ga4 적용

--- src/index.js
+++ src/index.js
@@ -8,17 +8,24 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from "redux-persist";
 import App from "./App";
 import {register} from './serviceWorker';
+import GA4React from "ga-4-react";
+
+const ga4react = new GA4React("G-6Y0ZL44ETQ");
 
 const persistor = persistStore(store);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App></App>
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
-);
+(async () => {
+  await ga4react.initialize();
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App></App>
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
